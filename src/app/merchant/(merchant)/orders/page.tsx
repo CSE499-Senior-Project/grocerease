cec645acc78d0f99ca33125ca3bd0d2a9a7d91a7
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2026-08-03 19:35:36 -0600

    Started to work on the front end.

--- src/app/merchant/(merchant)/orders/page.tsx
+++ src/app/merchant/(merchant)/orders/page.tsx
@@ -23,27 +23,18 @@ export default async function OrdersPage() {
         </OrderCard>
       ) : (
         <div className="space-y-4">
-          {orders.map((address) => (
-            <OrderCard key={address.id} className="p-4 md:p-6">
+          {orders.map((order) => (
+            <OrderCard key={order.id} className="p-4 md:p-6">
               <div className="flex items-start justify-between gap-4">
-                {/* <div>
-                  {address.is_default && (
-                    <span className="mb-2 inline-block rounded-full bg-brand-primary px-3 py-1 text-xs font-semibold text-white">
-                      Default
-                    </span>
-                  )}
-                  <p className="font-bold text-slate-900">{address.full_name}</p>
-                  <p className="text-slate-700">{address.address_1}</p>
-                  {address.address_2 && <p className="text-slate-700">{address.address_2}</p>}
-                  <p className="text-slate-700">
-                    {address.city}, {address.state} {address.zip_code}
-                  </p>
+                <div>
+                  <h3 className="font-bold text-slate-900">{order.profiles?.first_name}</h3>
+                  <p className="text-slate-700">{order.order_items.products.product_name}</p>
+                  <p className="text-slate-700">{order.delivery_address}</p>
                 </div>
-                <div className="flex shrink-0 gap-2">
-                  <Link href={`/account/address/${address.id}/edit`} className={editLinkClasses}>
+                {/* <div className="flex shrink-0 gap-2">
+                  <Link href={`/merchant/order/${order.id}/edit`} className={editLinkClasses}>
                     Edit
                   </Link>
-                  <DeleteAddressButton id={address.id} />
                 </div> */}
               </div>
             </OrderCard>
